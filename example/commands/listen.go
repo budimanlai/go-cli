3035735b48882ff578204020354bdb6e87873799
budimanlai <budiman.lai@gmail.com>
2024-12-28 21:37:02 +0700

    update

--- example/commands/listen.go
+++ example/commands/listen.go
@@ -1,6 +1,8 @@
 package commands
 
 import (
+	"time"
+
 	gocli "github.com/budimanlai/go-cli"
 )
 
@@ -16,18 +18,18 @@ func Listen(c *gocli.Cli) {
 			c.Log("Service in loop... ", number)
 			number = number + 1
 		},
-		// OnShutdown: func() {
-		// 	c.Log("Shutdown service detected...")
-		// 	c.Log("Cache cleanup....")
-		// 	time.Sleep(3 * time.Second)
+		OnShutdown: func() {
+			c.Log("Shutdown service detected...")
+			c.Log("Cache cleanup....")
+			time.Sleep(3 * time.Second)
 
-		// 	c.Log("Disconnect database...")
-		// 	time.Sleep(1 * time.Second)
+			c.Log("Disconnect database...")
+			time.Sleep(1 * time.Second)
 
-		// 	c.Log("Other process before stop the service...")
-		// 	time.Sleep(4 * time.Second)
+			c.Log("Other process before stop the service...")
+			time.Sleep(4 * time.Second)
 
-		// 	c.Log("Ok, now stop the service...")
-		// },
+			c.Log("Ok, now stop the service...")
+		},
 	})
 }
